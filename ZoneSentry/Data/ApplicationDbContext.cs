b676d589f3cf84ae5e9215f65157dae518970d8f
Neisvestney <neisvestney113@gmail.com>
2022-07-22 14:13:46 +0300

    Intitial commit

---
+++ ZoneSentry/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ZoneSentry.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace ZoneSentry.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+{
+    // public DbSet<Example> Examples { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
---
+++ ZoneSentry/Models/Response.cs
@@ -0,0 +1,7 @@
+﻿namespace ZoneSentry.Models;
+
+public class Response
+{
+    public string? Status { get; set; }
+    public string? Message { get; set; }
+}
